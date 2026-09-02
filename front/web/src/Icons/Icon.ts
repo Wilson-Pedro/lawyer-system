65c831bc9097d9760e83e89598a6e2c827529030
kawanHenrick <caminhakawan@gmail.com>
2026-09-02 02:43:26 -0300

    feat(icons):Inclui novos icons

--- front/web/src/Icons/Icon.ts
+++ front/web/src/Icons/Icon.ts
@@ -1,11 +1,12 @@
 import {
   FaEye,
+  FaEyeSlash,
   FaUsers,
   FaUserPlus,
   FaGavel,
   FaFileAlt,
   FaSignOutAlt,
-  FaEdit,
+  FaEdit, //
   FaPlus,
   FaArrowLeft,
   FaSave,
@@ -13,24 +14,65 @@ import {
   FaPlusCircle,
   FaUserCog,
   FaComment,
+  FaLock, // <-- Ícone da imagem adicionado aqui
+  FaUnlock,
+  FaUser,
+  FaUserEdit,
+  FaEnvelope,
+  FaKey,
+  FaTrashAlt,
+  FaTrash,
+  FaCheck,
+  FaTimesCircle,
+  FaTimes,
+  FaHome,
+  FaCog,
+  FaFilter,
+  FaEllipsisV,
+  FaBell,
+  FaExclamationTriangle,
 } from 'react-icons/fa';
 
 import { FaFileCirclePlus } from 'react-icons/fa6';
 
 export const EyeIcon = FaEye as React.FC<{ className?: string }>;
+export const EyeSlashIcon = FaEyeSlash as React.FC<{ className?: string }>;
 export const UsersIcon = FaUsers as React.FC<{ className?: string }>;
 export const UserPlusIcon = FaUserPlus as React.FC<{ className?: string }>;
 export const GavelIcon = FaGavel as React.FC<{ className?: string }>;
 export const FileAltIcon = FaFileAlt as React.FC<{ className?: string }>;
-export const SingOutAltIcon = FaSignOutAlt as React.FC<{ className?: string }>;
+export const SignOutAltIcon = FaSignOutAlt as React.FC<{ className?: string }>;
 export const EditIcon = FaEdit as React.FC<{ className?: string }>;
 export const PlusIcon = FaPlus as React.FC<{ className?: string }>;
 export const ArrowLeftIcon = FaArrowLeft as React.FC<{ className?: string }>;
 export const SaveIcon = FaSave as React.FC<{ className?: string }>;
-export const SeacrhIcon = FaSearch as React.FC<{ className?: string }>;
+export const SearchIcon = FaSearch as React.FC<{ className?: string }>;
 export const FileCirclePlusIcon = FaFileCirclePlus as React.FC<{
   className?: string;
 }>;
 export const PlusCircleIcon = FaPlusCircle as React.FC<{ className?: string }>;
 export const UserCogIcon = FaUserCog as React.FC<{ className?: string }>;
 export const CommentIcon = FaComment as React.FC<{ className?: string }>;
+
+// Novas exportações adicionadas abaixo:
+export const LockIcon = FaLock as React.FC<{ className?: string }>;
+export const UnlockIcon = FaUnlock as React.FC<{ className?: string }>;
+export const UserIcon = FaUser as React.FC<{ className?: string }>;
+export const UserEditIcon = FaUserEdit as React.FC<{ className?: string }>;
+export const EnvelopeIcon = FaEnvelope as React.FC<{ className?: string }>;
+export const KeyIcon = FaKey as React.FC<{ className?: string }>;
+export const TrashAltIcon = FaTrashAlt as React.FC<{ className?: string }>;
+export const TrashIcon = FaTrash as React.FC<{ className?: string }>;
+export const CheckIcon = FaCheck as React.FC<{ className?: string }>;
+export const TimesCircleIcon = FaTimesCircle as React.FC<{
+  className?: string;
+}>;
+export const TimesIcon = FaTimes as React.FC<{ className?: string }>;
+export const HomeIcon = FaHome as React.FC<{ className?: string }>;
+export const CogIcon = FaCog as React.FC<{ className?: string }>;
+export const FilterIcon = FaFilter as React.FC<{ className?: string }>;
+export const EllipsisVIcon = FaEllipsisV as React.FC<{ className?: string }>;
+export const BellIcon = FaBell as React.FC<{ className?: string }>;
+export const ExclamationTriangleIcon = FaExclamationTriangle as React.FC<{
+  className?: string;
+}>;
